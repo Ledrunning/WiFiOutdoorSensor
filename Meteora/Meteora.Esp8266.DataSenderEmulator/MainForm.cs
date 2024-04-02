d68199554710bdb936a60a179ed4880144e075dc
Ledrunning <ledrunner@hotmail.com>
2024-04-02 22:42:41 +0200

    Added ESP emulator project

---
+++ Meteora/Meteora.Esp8266.DataSenderEmulator/MainForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Meteora.Esp8266.DataSenderEmulator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
