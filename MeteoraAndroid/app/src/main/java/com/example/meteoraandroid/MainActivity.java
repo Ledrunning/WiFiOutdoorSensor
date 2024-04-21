991c30169e42a8f1f329ff912a8ec643589c8978
Ledrunning <ledrunner@hotmail.com>
2024-04-21 22:20:59 +0200

    Changed Android main activity code

--- MeteoraAndroid/app/src/main/java/com/example/meteoraandroid/MainActivity.java
+++ MeteoraAndroid/app/src/main/java/com/example/meteoraandroid/MainActivity.java
@@ -13,70 +13,39 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.Arrays;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
 
     private RequestQueue mRequestQueue;
     private StringRequest mStringRequest;
     private static final String TAG = MainActivity.class.getName();
-    List<String> routings = Arrays.asList("/temperature", "/humidity", "/altitude", "/pressure", "/battery_status");
-    private String url = "http://192.168.0.101/";
+    private String url = "http://192.168.0.101:8080";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sendAndRequestResponse();
     }
 
     private void sendAndRequestResponse() {
 
-        //RequestQueue initialized
         mRequestQueue = Volley.newRequestQueue(this);
 
-        //String Request initialized
         mStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-
-                Toast.makeText(getApplicationContext(),"Response :" + response.toString(), Toast.LENGTH_LONG).show();//display the response on screen
-
+                Toast.makeText(getApplicationContext(), "Response: " + response, Toast.LENGTH_LONG).show();
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
 
-                Log.i(TAG,"Error :" + error.toString());
+                // Обработка ошибки
+                Log.e(TAG, "Error: " + error.toString());
             }
         });
 
         mRequestQueue.add(mStringRequest);
     }
-
-
-    /*private void runThread() {
-
-        new Thread() {
-            public void run() {
-                while (i++ < 1000) {
-                    try {
-                        runOnUiThread(new Runnable() {
-
-                            @Override
-                            public void run() {
-                                btn.setText("#" + i);
-                            }
-                        });
-                        Thread.sleep(300);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }.start();
-    } */
 }
